687f50a9c64752c90d183a5e8a275e384f1f3904
Shiv Pratap Singh <shiv.si@media.net>
2019-10-12 14:09:06 +0530

    one more problem solved

---
+++ 113.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double base, num;
+    while(scanf("%lf\n%lf",&base,&num) != EOF){
+        printf("%.0lf\n",pow(num,1.0/base));
+    }
+    return 0;
+}
