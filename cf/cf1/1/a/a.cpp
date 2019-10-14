1fc13fb2bb439a0629846b9fd20eb79da02e56b2
Shiv Pratap Singh <shiv.si@media.net>
2019-10-15 00:26:46 +0530

    one more problem solved

--- cf/cf1/1/a/a.cpp
+++ cf/cf1/1/a/a.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-
+    double n,m,a;
+    cin >> n >> m >> a;
+    double l = ceil(n/a);
+    double w = ceil(m/a);
+    long long ans = l * w;
+    cout << ans << endl;
     return 0;
 }    
