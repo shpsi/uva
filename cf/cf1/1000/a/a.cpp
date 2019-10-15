3a0c041aee0b704abfb9442b129a7ce308bcdc47
Shiv Pratap Singh <shiv.si@media.net>
2019-10-15 22:51:46 +0530

    practice

--- cf/cf1/1000/a/a.cpp
+++ cf/cf1/1000/a/a.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-
+    int n;
+    cin>>n;
+    map<string,int> mp;
+    string s;
+    for(int i=0;i<n;++i){
+        cin >>s;
+        mp[s]++;
+    }
+    int ans = 0;
+    for(int i=0;i<n;++i){
+        cin >> s;
+        if(mp[s] > 0)
+            mp[s]--;
+        else
+            ans++;
+    }
+    cout<<ans;
     return 0;
 }    
