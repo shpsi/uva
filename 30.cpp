9f5c225d66a3c8e820f425751a576760b2cd3efa
Shiv Pratap Singh <shiv.si@media.net>
2019-10-20 19:40:10 +0530

    problem id 30 fixed

--- 30.cpp
+++ 30.cpp
@@ -5,21 +5,20 @@ typedef long long ll;
 int divideArray(int arr[], int n, int k){
     int dp[n+1][k+1];
     memset(dp,INT_MAX,sizeof(dp));
-    int prefixSum = 0, preMax=INT_MIN;
-    for(int i=0;i<n;++i){
-        prefixSum += arr[i];
-        preMax=max(preMax,arr[i]);
-        dp[i][0] = preMax*(i)-prefixSum;
-    }
-    for(int i=1;i<n;++i){
-        for(int part=1;part<=k;++part){
-            int runningSum = arr[i];
-            int runningMax = arr[i];
-            for(int j =i-1; j>=0;--j){
-                runningSum += arr[j];
-                runningMax = max(runningMax,arr[j]);
-                //cerr<<"debug:: "<<i<<" "<<j<<" "<<part<<" "<<runningSum<<" "<<runningMax<<endl;
-                dp[i][part] = min(dp[i][part], runningMax*(i-j+1)-runningSum+dp[j][part-1]);
+    k--;
+    for(int i=n-1;i>=0;--i){
+        for(int j=0;j<=k;++j){
+            dp[i][j] = INT_MAX;
+            int runningSum = 0;
+            int runningMax = INT_MIN;
+            for(int l = i; l<n;++l){
+                runningSum += arr[l];
+                runningMax = max(runningMax, arr[l]);
+                int diff = (l - i + 1) * runningMax - runningSum;
+                if(j>0)
+                    dp[i][j] = min(dp[i][j], diff + dp[l+1][j-1]);
+                else
+                    dp[i][j] = diff;
             }
         }
     }
@@ -29,7 +28,7 @@ int divideArray(int arr[], int n, int k){
         }
         cout<<endl;
     }
-    return dp[n-1][k];
+    return dp[0][k];
 }
 int main(){
     int arr[] = { 2, 9, 5, 4, 8, 3, 6 }; 
