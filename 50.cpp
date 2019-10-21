6db91dac4101a3a32c28d1126f2bfba604d33de4
Shiv Pratap Singh <shiv.si@media.net>
2019-10-21 14:35:11 +0530

    50 yet to be fixed

---
+++ 50.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int dp[3000][3000];
+bool taken[3000][3000];
+int maxScore(vector<int> a){
+    int n = a.size();
+    int totTurns = n;
+    bool turn = tot
+}
+string getMoves(int n){
+
+}
+int main(){
+    int ar[] = { 10, 80, 90, 30 }; 
+    int arraySize = sizeof(ar) / sizeof(int); 
+  
+    vector<int> bags(ar, ar + arraySize); 
+    int ans = maxScore(bags); 
+  
+    cout << ans << " " << getMoves(bags.size()) << endl; 
+    return 0;
+}
