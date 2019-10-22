643a7b9307f45e4e921f04f7d71fae268234ad32
Shiv Pratap Singh <shiv.si@media.net>
2019-10-22 15:48:30 +0530

    yet to fix 76

---
+++ 76.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/find-all-unique-pairs-of-maximum-and-second-maximum-elements-over-all-sub-arrays-in-onlogn/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
