5b79fe6d8d0196fe1bf63f1e70c8368d3e29814f
Shiv Pratap Singh <shiv.si@media.net>
2019-10-22 15:47:53 +0530

    yet to fix 75

---
+++ 75.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/number-of-possible-permutations-when-absolute-difference-between-number-of-elements-to-the-right-and-left-are-given/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
