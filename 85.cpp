b0b9708dd6e8bfa4335cdc3bfdd0c5473835054c
Shiv Pratap Singh <shiv.si@media.net>
2019-10-22 17:27:01 +0530

    yet to fix 85

---
+++ 85.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/maximum-bitwise-and-value-of-subsequence-of-length-k/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
