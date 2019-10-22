9c06960420a29f7ffbf25c6528d432bf23eec833
Shiv Pratap Singh <shiv.si@media.net>
2019-10-23 00:37:44 +0530

    yet to fit this

---
+++ 110.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/partition-the-array-in-k-segments-such-that-bitwise-and-of-individual-segment-sum-is-maximized/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
