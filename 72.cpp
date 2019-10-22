c74bb97af662b32395949c01f022edf6d061b246
Shiv Pratap Singh <shiv.si@media.net>
2019-10-22 15:32:01 +0530

    yet to fix 72

---
+++ 72.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/maximum-xor-of-two-numbers-in-an-array/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
