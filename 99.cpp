d2678df603fdc5532231e41b7f86e35529b1c5d2
Shiv Pratap Singh <shiv.si@media.net>
2019-10-22 22:20:34 +0530

    yet to fix 99

---
+++ 99.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/alternate-xor-operations-on-sorted-array/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
