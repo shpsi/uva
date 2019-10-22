966817e76984f3bbab9beffc50b7d4de122a7b5f
Shiv Pratap Singh <shiv.si@media.net>
2019-10-22 22:21:04 +0530

    yet to fix 100

---
+++ 100.cpp
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/find-array-such-that-no-subarray-has-xor-zero-or-y/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+int main(){
+    
+    return 0;
+}
