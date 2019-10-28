0e1ebb7cc73c51c0ee9dc8584826c9f2c8e90136
Shiv Pratap Singh <shiv.si@media.net>
2019-10-28 18:51:17 +0530

    7 a ac

--- 7/a/a.cpp
+++ 7/a/a.cpp
@@ -1,37 +1,41 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<string, int> psi;
-typedef pair<string, string> pss;
-#define pb push_back
-#define PI acos(-1.0)
-#define EPS 1e-4
-#define mp make_pair
-#define all(x) x.begin(), x.end()
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define fout(S,x)            cout<<fixed<<setprecision(x)<<(S)<<endl
-#define present(c,x) ((c).find(x) != (c).end())
-#define T() ll t;cin>>t;while(t--)
-#define mem(input,b) memset(input,b,sizeof(input))
-#define ff first
-#define ss second
-#define fread(input) freopen("input","r",stdin)
-#define fwrite(b) freopen("b","w",stdout)
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-#define FOREACH(i,c) for (__typeof ((c).begin()) i = (c).begin() ; i != (c).end() ; i++)
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1){
-    cerr << name << " : " << arg1 << std::endl;
-
-}
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args){
-    const char* comma = strchr(names + 1, ',');cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
-
-}
 int main(){
-    fastio;
-
-    return 0;
-}    
+	char a[8][8];
+	int flag[8]={0};
+	int flag1[8]={0};
+	int i,j;
+	for(i=0;i<8;i++){
+		for(j=0;j<8;j++){
+			cin>>a[i][j];
+		}
+	}
+	for(i=0;i<8;i++){
+		for(j=0;j<8;j++){
+			if(a[i][j]=='B') flag[i]=1;
+			else{
+			flag[i]=0;
+			break;	
+			} 
+		}
+	}
+	for(j=0;j<8;j++){
+		for(i=0;i<8;i++){
+			if(a[i][j]=='B') flag1[j]=1;
+			else {
+				flag1[j]=0;
+				break;
+			}
+		}
+	}
+	int sum=0;
+	for(i=0;i<8;i++){
+		if(flag[i]==1) sum++;
+	}
+	for(i=0;i<8;i++){
+		if(flag1[i]==1) sum++;
+	}
+	if(sum==16) cout<<"8\n";
+	else cout<<sum<<endl;
+	return 0;
+} 
