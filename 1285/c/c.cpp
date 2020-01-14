fb3a7e15f0a17713cc1d2e8becf6dfe5d55cf0db
Shiv Pratap Singh <shiv.si@media.net>
2020-01-15 01:52:52 +0530

    passed

---
+++ 1285/c/c.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<string, string> pss;
+#define pb push_back
+#define PI acos(-1.0)
+#define EPS 1e-4
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fout(S,x)            cout<<fixed<<setprecision(x)<<(S)<<endl
+#define present(c,x) ((c).find(x) != (c).end())
+#define T() ll t;cin>>t;while(t--)
+#define mem(input,b) memset(input,b,sizeof(input))
+#define ff first
+#define ss second
+#define fread(input) freopen("input","r",stdin)
+#define fwrite(b) freopen("b","w",stdout)
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define FOREACH(i,c) for (__typeof ((c).begin()) i = (c).begin() ; i != (c).end() ; i++)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+    cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+    const char* comma = strchr(names + 1, ',');cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+ll gcd(ll a, ll b){
+    return a==0?b:gcd(b%a,a);
+}
+int main(){
+    fastio;
+    ll x;
+    cin>>x;
+    for(ll i=sqrt(x);i>=1;--i){
+        ll j=x/i;
+        ll g = gcd(i,j);
+        ll rhs = (i*j)/g;
+        if(rhs == x){
+            cout<<i<<" "<<j<<endl;
+            return 0;
+        }
+    }
+    cout<<1<<" "<<x<<endl;
+    return 0;
+}
